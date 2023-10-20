215136204331c5fdcbfdd4611830619479141663
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-21 00:54:56 +0300

    Update BooksList.js

--- reduxt-toolkit-thunk-book-app/src/components/Book/BooksList.js
+++ reduxt-toolkit-thunk-book-app/src/components/Book/BooksList.js
@@ -2,23 +2,24 @@ import React from "react";
 
 const BooksList = ({ isLoading, books }) => {
   const booksList =
-    books &&
-    books.map((item) => (
-      <li
-        className="list-group-item d-flex  justify-content-between align-items-center"
-        key={item.id}
-      >
-        <div>{item.title}</div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary">
-            Read
-          </button>
-          <button type="button" className="btn btn-danger">
-            Delete
-          </button>
-        </div>
-      </li>
-    ));
+    books.length > 0
+      ? books.map((item) => (
+          <li
+            className="list-group-item d-flex  justify-content-between align-items-center"
+            key={item.id}
+          >
+            <div>{item.title}</div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">
+                Read
+              </button>
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </li>
+        ))
+      : "NO Books Available";
 
   return (
     <div>
