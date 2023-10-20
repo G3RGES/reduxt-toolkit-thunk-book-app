485f3f3245bb9b5f50238ade85f7245cd175a8dd
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-21 01:48:05 +0300

    Update AddForm.js

--- reduxt-toolkit-thunk-book-app/src/components/AddForm.js
+++ reduxt-toolkit-thunk-book-app/src/components/AddForm.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { insertBooks } from "../store/bookSlice";
 
 const Addform = () => {
+  const title = useRef(null);
+  const price = useRef(null);
+  const description = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      // id: Math.random().toString(),
+      title: title.current.value,
+      price: price.current.value,
+      description: description.current.value,
+    };
+    dispatch(insertBooks(data));
+    // console.log("form");//TESTING
+    // console.log(title.current.value); //TESTING
+  };
+
   return (
-    <div className='row'>
-      <div className='col-6 offset-3 mt-3'>
+    <div className="row">
+      <div className="col-6 offset-3 mt-3">
         <h2>Insert Book</h2>
-        <form>
-          <div className='form-group'>
-            <label htmlFor='title'>Title</label>
-            <input type='text' className='form-control' id='title' required />
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              required
+              ref={title}
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='price'>Price</label>
-            <input type='number' className='form-control' id='price' required />
+          <div className="form-group">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              required
+              ref={price}
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='Description'>Description</label>
+          <div className="form-group">
+            <label htmlFor="Description">Description</label>
             <textarea
-              className='form-control'
-              id='Description'
-              rows='3'
+              ref={description}
+              className="form-control"
+              id="Description"
+              rows="3"
               required
             ></textarea>
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
