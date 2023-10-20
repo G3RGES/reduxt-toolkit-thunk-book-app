31e167d38839e413e866200dd0582290133aa032
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-20 03:22:48 +0300

    Update bookSlice.js

--- reduxt-toolkit-thunk-book-app/src/store/bookSlice.js
+++ reduxt-toolkit-thunk-book-app/src/store/bookSlice.js
@@ -1,14 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const getBooks = createAsyncThunk("book/getBooks", async (_, thunkAPI) => {
-  try {
-    const res = await fetch("http://localhost:3009/books");
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+export const getBooks = createAsyncThunk(
+  "book/getBooks",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:3009/books");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 const bookSlice = createSlice({
   name: "book",
